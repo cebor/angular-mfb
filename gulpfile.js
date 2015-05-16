e155b87053ef130a71a3901c3e4efbb4cd1c38e7
Felix Itzenplitz <felix@stkn.org>
2015-05-16 10:59:07 +0200

    Add gulp bump

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var git = require('gulp-git');
+var bump = require('gulp-bump');
+var tag_version = require('gulp-tag-version');
 
 gulp.task('styles', function() {
   gulp.src('./src/styles/*.scss')
@@ -21,3 +24,20 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('build', ['styles', 'scripts']);
+
+function inc(importance) {
+  // get all the files to bump version in
+  gulp.src('./bower.json')
+    // bump the version number in those files
+    .pipe(bump({type: importance}))
+    // save it back to filesystem
+    .pipe(gulp.dest('./'))
+    // commit the changed version number
+    .pipe(git.commit('Bump bower version'))
+    // **tag it in the repository**
+    .pipe(tag_version());
+}
+
+gulp.task('bump:patch', function() { return inc('patch'); });
+gulp.task('bump:minor', function() { return inc('minor'); });
+gulp.task('bump:major', function() { return inc('major'); });
